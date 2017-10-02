797ba56c418837ffe0ea76b455c7ac56abc3d932
Helen Yi <helenhyi@gmail.com>
2017-10-02 16:00:54 -0700

    Add getAllSurveys, createSurvey, and createUser.

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -4,14 +4,42 @@ const mysqlConfig = require('./config.js');
 const connection = mysql.createConnection(mysqlConfig);
 
 // function to get all surveys
-
+var getAllSurveys = function(callback) {
+  connection.query('select * from Survey', function(err, results, fields) {
+    if (err) {
+      console.log(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
 
 // function to update survey table with new survey
+var createSurvey = function(params, callback) {
+  connection.query('insert into Survey set ?', params, function(err, results, fields) {
+    if (err) {
+      console.log(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
 
+// function to update survey information 
 
 // function to create new user
-
+var createUser = function(params, callback) {
+  connection.query('insert into Users set ?', params, function(err, results, fields) {
+    if (err) {
+      console.log(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
 
 module.exports = {
-
+  getAllSurveys,
+  createSurvey,
+  createUser
 };
